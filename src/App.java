2329470de1b2d69eed3902121cb72828ed2382ab
vinicarpes <vinicdf000@gmail.com>
2024-10-14 22:07:02 -0300

    logic improvement

--- src/App.java
+++ src/App.java
@@ -17,16 +17,22 @@ public class App {
         for (int i = 0; i < 10; i++) { // Atribuindo 0 a todas as posições
             seats[i] = 0;
         }
-
+        
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("##########################################");
-        System.out.println("MENU DE SELEÇÃO\nPressione 1 para Primeira Classe.\nPressione 2 para Classe Executiva.\nPressione 3 paraver os assentos disponíveis");
-        System.out.println("##########################################");
-
+        
         while (isEmpty) {
+            System.out.println("\nMENU DE SELEÇÃO\nPressione 1 para Primeira Classe.\nPressione 2 para Classe Executiva.\nPressione 3 para ver os assentos disponíveis");
+            System.out.println();
             int choice = scanner.nextInt();
-
+            
+            // Verificar se o vetor todo equivale a 1
+            int soma = 0;
+            for (int i = 0; i < 10; i++) {
+                soma += seats[i];
+                if (soma == 10) {
+                    isEmpty = false;
+                }
+            }
             switch (choice) {
                 case 1:
                     boolean seatAssigned = false;
@@ -51,7 +57,7 @@ public class App {
                                 }
                             }
                             if (!seatAssigned) {
-                                System.out.println("CLASSE EXECUTIVA está lotada. Aguarde o próximo vôo.");
+                                System.out.println("CLASSE EXECUTIVA também está lotada. Aguarde o próximo vôo.");
                             }
                         } else {
                             System.out.println("Aguarde o próximo vôo.");
@@ -70,7 +76,7 @@ public class App {
                         }
                     }
                     if (!seatAssigned) {
-                        System.out.println("CLASSE EXECUTIVA lotada. Você deseja ser alocado(a) na PRIMEIRA CLASSE?  \n1 - Sim \n2- Não");
+                        System.out.println("CLASSE EXECUTIVA lotada. Você deseja ser alocado(a) na PRIMEIRA CLASSE?  \n1 - Sim \n2 - Não");
                         int anwser = scanner.nextInt();
                         if (anwser == 1) {
                             for (int i = 0; i < 5; i++) {
@@ -82,7 +88,7 @@ public class App {
                                 }
                             }
                             if (!seatAssigned) {
-                                System.out.println("PRIMEIRA CLASSE lotada. Aguarde o próximo vôo.");
+                                System.out.println("PRIMEIRA CLASSE também está lotada. Aguarde o próximo vôo.");
                             }
                         } else {
                             System.out.println("Aguarde o próximo vôo.");
@@ -95,25 +101,29 @@ public class App {
                     System.out.println("ASSENTOS LIVRES\nPRIMEIRA CLASSE: \n");
                     for (int i = 0; i < 5; i++) {
                         int aux =0;
-                        if(seats[i]==0){
-                            System.out.println("Assento n° " + (i+1));
+                        if (aux !=4) {
+                            if(seats[i]==0){
+                                System.out.println("Assento n° " + (i+1));
+                            }
+                        }else{
+                            System.out.println("Não há assentos disponíveis!! Aguarde o próximo vôo.");
                         }
+                        aux++;
                     }
                     System.out.println("CLASSE EXECUTIVA: \n");
                     for (int i = 5; i < 10; i++) {
-                        if(seats[i]==0){
-                            System.out.println("Assento n° " +(i+1));
+                        int aux =0;
+                        if (aux<4) {
+                            if(seats[i]==0){
+                                System.out.println("Assento n° " +(i+1));
+                            }
+                        }else{
+                            System.out.println("Não há assentos disponíveis!! Aguarde o próximo vôo.");
+                        }
+                        aux++;
                     }
-            }
+                    break;
 
-            // Verificar se o vetor todo equivale a 1
-            int soma = 0;
-            for (int i = 0; i < 10; i++) {
-                soma += seats[i];
-                if (soma == 10) {
-                    isEmpty = false;
-                }
-            }
         }
     }
         scanner.close();
